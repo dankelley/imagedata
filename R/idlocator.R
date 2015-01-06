50d7a3cf945a7f4c02b7d27c06d14f08bc184a0c
Dan Kelley <kelley.dan@gmail.com>
2015-01-06 19:22:08 -0400

    code refactoring

--- R/idlocator.R
+++ R/idlocator.R
@@ -1,18 +1,37 @@
-## Find data, with undo and stop regions.
+#' Digitize a point, or UNDO, or STOP.
+#'
+#' @param S information on the STOP region
+#' @param U information on the UNDO region
+#' @param X information on the x scale
+#' @param Y information on the y scale
+#' @param bell TRUE to ring the bell with mouse clicks
+#' @param verbose TRUE to print more information
+#' @param type 'p' for points, 'n' for none, etc as for \code{\link{plot}}
+#' @param col colour (ignored unless plotting) as for \code{\link{par}}
+#' @param lwd line width of symbols as for \code{\link{par}}
+#' @param pch symbol code as for \code{\link{par}}
+#' @param cex symbol size expansion factor as 0for \code{\link{par}}
+#'
+#' @return a list containing digitized values \code{x} and \code{y} along with \code{status}, which may be \code{"DATA"} for data, \code{"AXIS"} for axis information, \code{"STOP"} for a click in the STOP region, or \code{"UNDO"} for a click in the UNDO region.
+#' plus other properties 
+#' @export idlocator
 idlocator <- function(S=NULL, U=NULL,
                       X=NULL, Y=NULL,
-                      col='red', pch=20, cex=1/2,
-                      bell=TRUE, verbose=TRUE)
+                      bell=TRUE, verbose=TRUE,
+                      type='p', col='red', lwd=par("lwd"), pch=20, cex=1/2)
 {
-    xy <- locator(1, type='p', col=col, pch=pch, cex=cex)
+    xy <- locator(1, type='n', col=col, pch=pch, cex=cex)
     if (bell) alarm()
     if (!is.null(S) && (xy$x > S$x && xy$y > S$y))
         return(list(x=xy$x, y=xy$y, status="STOP"))
     if (!is.null(U) && (xy$x < U$x && xy$y < U$y))
         return(list(x=NULL, y=NULL, status="UNDO"))
     # OK, must be a data point, but it could be actual data, or axis data.
-    if (!is.null(X) && !is.null(Y))
+    if (type == "p") 
+        points(xy$x, xy$y, col=col, pch=pch, cex=cex)
+    if (!is.null(X) && !is.null(Y)) {
         return(list(x=as.numeric(X$a + X$b * xy$x),
                     y=as.numeric(Y$a + Y$b * xy$y), status="DATA"))
+    }
     return(list(x=xy$x, y=xy$y, status="AXIS"))
 }
--- R/imagedata.R
+++ R/imagedata.R
@@ -1,51 +1,52 @@
 #' Digitize a linear-scale plot that is shown in a graphics window.
 #'
-#' 'imagedata', the main function of the 'imagedata' package,  
+#' @description
+#' \code{imagedata}, the main function of the 'imagedata' package,  
 #' digitizes points from images.  As of now, the images must
-#' be in PNG format, and the axes must be linear.
+#' be in PNG format, and the axes must be linear. Also, at
+#' least on OS X machines, the plot device type must be 
+#' quartz, since x11 does not properly buffer text in the console
+#' when combined with clicks in the plot window.
 #'
-#' If 'rotated' is TRUE, then the first step is to display the image
+#' @details
+#' If \code{rotated} is TRUE, then the first step is to display the image
 #' with a red diagonal line and red dotted horizontal and vertical lines,
 #' inviting the user to click above or below the diagonal line to 
 #' alter the rotation angle.  Any click that is to the left of the
 #' centre of the image will end this step, storing the inferred angle.
 #' The idea of this interactive method is to help in cases of flawed
 #' documents, e.g. those created with a photocopier.
+#'
+#' Then the user is asked to click somewhere at the bottom-left of 
+#' image, to set up an "UNDO" zone, and then the top-right, to
+#' set up a "STOP" zone. Normally these zones are placed outside
+#' the axis; they only have to be large enough to be easily clicked
+#' within.
 #' 
 #' Next, the user asked to click on the x axis, at values given
-#' by the 'xaxis' argument.  These values are normally at tick
+#' by the \code{xaxis} argument.  These values are normally at tick
 #' marks.  The same is then done for the y axis.
 #'
-#' Then the user is asked to click at the top-right of the plotting
-#' area.  Actually, it can be a bit outside the area, if desired, or
-#' inside it.  This establishes a "stop" zone, which will be drawn
-#' on the plot.  Once the actual digitization is started, clicking
-#' in this zone ends the processing.
+#' Then it's time to digitize image data. If a mistake is made,
+#' click in the UNDO zone. To stop digitizing, click in the STOP
+#' zone.  The results of \code{imagedata} are a list, of which
+#' the two most interesting components are \code{x} and \code{y},
+#' the coordinates of the clicked points, translated to the 
+#' coordinates of the plot (i.e. using the axis information).
 #' 
-#' Then the user is asked to click to the lower-left of the plot
-#' to establish an "undo" zone.  Clicking in that zone removes
-#' the most recently acquired point.
-#'
-#' Finally, the actual digitization process begins, with the
-#' user clicking within the plot region, using the "undo"
-#' zone to correct mistakes, and the "stop" zone to finish.
-#'
-#' Points that are clicked are indicated on the plot.
-#'
-#' To digitize a second curve with the same axes, one may 
-#' use 'getdata()', supplying it with 'X', 'Y', 'S' and 'U'
-#' values returend by 'imagedata'.
-#'
 #' @param image name of image file (must be PNG)
 #' @param xaxis values along the x axis, typically at tick marks
 #' @param yaxis values along the y axis, typically at tick marks
 #' @param rotated a logical indicating whether the axes are rotated
-#' @return a list containing 'x' and 'y' values, which are the
-#' digitized points, 'X' and 'Y', which are lists containing 'a'
-#' and 'b' values that hold the axis transformations, 'S', a
-#' list containing 'x' and 'y' defining the lower-left corner
-#' of the stop zone, and 'U', a list containing the 'x' and 'y'
-#' defining the upper-right corner of the undo zone.
+#' @param col colour of points, as for \code{\link{par}}
+#' @param pch type of points, as for \code{\link{par}}
+#' @param cex size of points, as for \code{\link{par}}
+#' @return \code{imagedata} returns a list containing \code{x} and \code{y} values, which are the
+#' digitized points, \code{X} and \code{Y}, which are lists containing \code{a}
+#' and \code{b} values that hold the axis transformations, \code{S}, a
+#' list containing \code{x} and \code{y} defining the lower-left corner
+#' of the STOP zone, and \code{U}, a list containing the \code{x} and \code{y}
+#' defining the upper-right corner of the UNDO zone.
 #'
 #' @import png
 #' @examples
@@ -66,7 +67,7 @@
 #' }
 #' @export imagedata
 
-imagedata <- function(image, xaxis, yaxis, rotated=FALSE)
+imagedata <- function(image, xaxis, yaxis, rotated=FALSE, col="red", pch=20, cex=1)
 {
     image <- readPNG(image)
     par(mar=rep(0, 4))
@@ -90,23 +91,23 @@ imagedata <- function(image, xaxis, yaxis, rotated=FALSE)
     showtopright(S)
     xx <- NULL
     for (x in xaxis) {
-        cat("Click on the x axis where x=", x, "\n")
+        cat("Click on the x axis where x =", x, "\n")
         flush.console()
-        xx <- c(xx, idlocator(bell=TRUE, col="red", pch=20, cex=1)$x)
+        xx <- c(xx, idlocator(bell=TRUE, type="p", col="red", pch=1, cex=1)$x)
     }
     ab <- as.numeric(coef(lm(xaxis ~ xx)))
     X <- list(a=ab[1], b=ab[2])
-    cat("X transformation: x=", X$a, " + ", X$b, "*xPixel\n")
+    cat("    (X transformation: x=", X$a, " + ", X$b, "*USRx)\n")
     flush.console()
     yy <- NULL
     for (y in yaxis) {
-        cat("Click on the y axis where y=", y, "\n")
+        cat("Click on the y axis where y =", y, "\n")
         flush.console()
-        yy <- c(yy, idlocator(bell=TRUE, col="blue", pch=20, cex=1)$y)
+        yy <- c(yy, idlocator(bell=TRUE, type="p", col="red", pch=1, cex=1)$y)
     }
     ab <- as.numeric(coef(lm(yaxis ~ yy)))
     Y <- list(a=ab[1], b=ab[2])
-    cat("Y transformation: y=", Y$a, " + ", Y$b, "*yPixel\n")
+    cat("    (Y transformation: y=", Y$a, " + ", Y$b, "*USRy)\n")
     flush.console()
     x <- y <- NULL
     n <- 0
@@ -114,17 +115,25 @@ imagedata <- function(image, xaxis, yaxis, rotated=FALSE)
     flush.console()
     ## FIXME: put loop here
     while (TRUE) {
-        xy <- getdata(X=X, Y=Y, S=S, U=U)
+        xy <- idlocator(X=X, Y=Y, S=S, U=U, col=col, pch=pch, cex=cex)
         if (xy$status=="STOP")
             break
         if (xy$status=="UNDO") {
             if (length(x)) {
+                cat("Removed datum #", n, "with x=", tail(x, 1), " and y=", tail(y, 1), "\n")
                 flush.console()
                 x <- head(x, -1)
                 y <- head(y, -1)
                 n <- n - 1
-                cat("Removed datum #", n, "with x=", tail(x, 1), " and y=", tail(y, 1), " [An upcoming version will erase the point]\n")
                 flush.console()
+                ## redraw
+                rasterImage(image, 0, 0, 1, 1)
+                showbottomleft(U)
+                showtopright(S)
+                ## FIXME: undo and stop
+                xusr<-(x - X$a) /X$b
+                yusr<-(y - Y$a) /Y$b
+                points(xusr, yusr, col=col, pch=pch, cex=cex)
             }
         } else if (xy$status=="DATA") {
             x <- c(x, xy$x)
